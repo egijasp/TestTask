f9e398938d17ad9942e17392b2506ea3dc4fafde
egijasp <egija.sprukule@gmail.com>
2024-07-05 18:19:42 +0300

    update about page layout

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,20 +3,14 @@ import myphoto from "../assets/photo.jpg";
 
 function AboutPage() {
   return (
-    <div id="aboutPage" className="container">
-      <div className="about_tilte">
-        <h2>About me</h2>
-        <p>Here will be information about me...</p>
-      </div>
+    <div id="aboutPage" className="container about">
       <div className="about_info">
-        <img className="photo" src={myphoto} alt="me" />
-        <div>
-          <h3>My name is Egija</h3>
-          <p>
-            I am a front-end developer. I have been working in this field for
-            more than 2 years. I have experience with React, Angular, and Vue.
-          </p>
+        <div className="about_hello">
+          <span>Hello!</span>
+          <br />
+          <span>I'm Egija</span>
         </div>
+        <img className="photo" src={myphoto} alt="me" />
       </div>
     </div>
   );
