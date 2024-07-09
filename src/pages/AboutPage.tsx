5286237f65a26693e76cc00bbd4903553d868739
egijasp <egija.sprukule@gmail.com>
2024-07-09 11:18:38 +0300

    updated aboutpage style

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,16 +3,18 @@ import myphoto from "../assets/photo.webp";
 
 function AboutPage() {
   return (
-    <div id="aboutPage" className="container about">
-      <div className="about_info">
-        <div className="about_hello">
-          <span>Hello!</span>
-          <br />
-          <span>I'm Egija</span>
+    <main className="main">
+      <div id="aboutPage" className="container about">
+        <div className="about_info">
+          <div className="about_hello">
+            <span>Hello!</span>
+            <br />
+            <span>I'm Egija</span>
+          </div>
+          <img className="photo" src={myphoto} alt="me" />
         </div>
-        <img className="photo" src={myphoto} alt="me" />
       </div>
-    </div>
+    </main>
   );
 }
 
